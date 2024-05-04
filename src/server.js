ed17aba6073a4078c9754a76022f1aee8549f911
zalfyputra <zalfyputra@gmail.com>
2024-05-04 09:56:09 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+// Open this link in a browser --> http://notesapp-v1.dicodingacademy.com/
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
